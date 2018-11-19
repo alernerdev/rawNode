468ec52527c6e20e3f83d9f14b8836ca9d029137
Lerner <Alex.Lerner@thomsonreuters.com>
2018-11-19 16:58:24 -0500

    added most of the CLI admin code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const server = require('./lib/server');
 const workers = require('./lib/workers');
+const cli = require('./lib/cli');
 
 // declare the app
 const app = {};
@@ -10,6 +11,12 @@ app.init = function init() {
 
     // start workers
     workers.init();
+
+    // launch cli last
+    setTimeout(function() {
+        cli.init();
+    }, 200);
+
 }
 
 app.init();
