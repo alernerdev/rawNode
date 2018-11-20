2950c9238919511992b5401e5e1b5c5ada0e9aec
Lerner <Alex.Lerner@thomsonreuters.com>
2018-11-20 14:27:29 -0500

    added performance hooks, clustering support, try/catch error handling

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const cluster = require('cluster');
+const os = require('os');
+
 const server = require('./lib/server');
 const workers = require('./lib/workers');
 const cli = require('./lib/cli');
@@ -6,17 +9,27 @@ const cli = require('./lib/cli');
 const app = {};
 
 app.init = function init() {
-    // start serrver
-    server.init();
 
-    // start workers
-    workers.init();
+    // workers and CLI should start on the master thread
 
-    // launch cli last
-    setTimeout(function() {
-        cli.init();
-    }, 200);
+    if (cluster.isMaster) {
+        // start workers
+        workers.init();
 
+        // launch cli last
+        setTimeout(function () {
+            cli.init();
+        }, 200);
+
+        // and forks should process all the http request traffic
+        for (let i=0; i< os.cpus().length; i++) {
+            cluster.fork();
+        }
+        
+    } else {
+        // servers get started on separate forked processes
+        server.init();
+    }
 }
 
 app.init();
